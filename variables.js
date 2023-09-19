68471bdfc1022f2921fbad2e3fdbfc97fe027804
Annabella <a.loiv@voco.ee>
2023-09-19 12:02:49 +0300

    js variables - declare, assign and use

---
+++ variables.js
@@ -0,0 +1,20 @@
+//variable
+//keyword variablename = value;
+
+//let
+
+//declare
+let programmingLanguage = 'Java Script'
+console.log(programmingLanguage)
+
+// assigment - save new value into the variables
+programmingLanguage = 'PHP'
+console.log(programmingLanguage)
+
+let result
+console.log(result)
+
+// const
+const group = 'VS23'
+//group = 'PG23' - not allowed
+console.log(group)
