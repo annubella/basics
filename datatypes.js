ac1a7fb983cbe3f38626bee6a889ce87a05c66be
Annabella <a.loiv@voco.ee>
2023-09-19 13:59:35 +0300

    Datatypes = string, numbers and conversion

---
+++ datatypes.js
@@ -0,0 +1,28 @@
+//data types
+//String
+let firstname = 'Annabella Eliisa'
+let lastname = 'Loiv'
+console.log(firstname + ' ' +lastname)
+console.log(typeof firstname)
+
+//numbers
+let goodMark = 5
+console.log(goodMark - 2)
+console.log(typeof goodMark)
+
+//boolean
+let isToday = true
+console.log(typeof isToday)
+
+//type conversion
+//number -> String
+console.log(String(5.5) + String(5.5))
+
+//boolean -> String
+console.log(String(true))
+
+//string -> number
+console.log(Number('420'))
+console.log(Number('5.25'))
+console.log(parseInt('10.8'))
+console.log(parseFloat('100.3'))
