2305e4d9fc1525fd72ad45f1f9f3c42a83625644
Annabella <a.loiv@voco.ee>
2023-09-26 13:13:18 +0300

    ylesanne

---
+++ yl1.2.js
@@ -0,0 +1,6 @@
+//ylesanne 1.2 President
+let aasta = 2021.
+let president = 'Alar Karis'
+let lauseKeskosa = 'aastal valiti Eesti presidendiks'
+
+console.log(aasta + '. ' + lauseKeskosa + ' ' + president)
